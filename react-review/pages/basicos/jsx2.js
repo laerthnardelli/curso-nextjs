f7f69c8abe10ce26db56f2bf312d360fcaa9e91f
laerthnardelli <laerthnardelli@gmail.com>
2021-10-14 11:05:11 -0300

    JSX #02

---
+++ react-review/pages/basicos/jsx2.js
@@ -0,0 +1,12 @@
+// JSX
+export default function jsx2() {
+  const conteudo = (
+    <div>
+      <h1>JSX #02</h1>
+    </div>
+  );
+  return conteudo;
+
+  //const conteudo = <div><h1>JSX #02</h1></div>;
+  //return <div><h1>JSX #02</h1></div>
+}
