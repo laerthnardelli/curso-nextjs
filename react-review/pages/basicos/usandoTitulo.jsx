dfbc465ab32821897a2499c702f5c27ea16c4685
laerthnardelli <laerthnardelli@gmail.com>
2021-10-14 15:10:32 -0300

    Property Component

--- react-review/pages/basicos/usandoTitulo.jsx
+++ react-review/pages/basicos/usandoTitulo.jsx
@@ -3,7 +3,20 @@ import Titulo from "../../components/Titulo";
 export default function usandoTitulo() {
     return (
         <div>
-            <Titulo />
+            <Titulo
+                principal="Página de Cadastro"
+                secundario="Incluir, alterar e excluir coisas!"
+            />
+            <Titulo
+                principal="Página de Login"
+                secundario="Informe o seu email e senha"
+                pequeno={true}
+            />
+            <Titulo
+                principal="Página de Login"
+                secundario="Informe o seu email e senha"
+                pequeno
+            />
         </div>
     );
 }
