e6725cbcc8387ff2d7f0535f7fd12b8a05c56d03
laerthnardelli <laerthnardelli@gmail.com>
2021-10-25 21:22:42 -0300

    Project Board (Tabuleiro) #02

--- tabuleiro/pages/index.jsx
+++ tabuleiro/pages/index.jsx
@@ -1,12 +1,11 @@
+import Linha from '../components/Linha';
 import Subdivisao from '../components/Subdivisao';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Subdivisao />
-      <Subdivisao />
-      <Subdivisao />
+      <Linha />
     </div>
   );
 }
