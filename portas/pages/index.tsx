aa761b0eecaa1d3be4cfeead5f2cf6a487145e47
laerthnardelli <laerthnardelli@gmail.com>
2022-01-04 16:30:49 -0300

    Adding Events to Port #03

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -10,7 +10,6 @@ export default function Home() {
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <input type="text" value={texto} onChange={e => setTexto(e.target.value)} />
       <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
 
     </div>
