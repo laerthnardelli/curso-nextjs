82cdd9daf78fce0ef56d35c48d9b056f5e1cebf6
laerthnardelli <laerthnardelli@gmail.com>
2022-01-04 20:50:11 -0300

    Using Router

---
+++ portas/pages/jogo/[portas]/[temPresente].tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import Porta from "../../../components/Porta";
+import { atualizarPortas, criarPortas } from "../../../functions/portas";
+import styles from "../../../styles/Jogo.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function jogo() {
+
+  const router = useRouter();
+
+  const [portas, setPortas] = useState([]);
+
+  useEffect(() => {
+    const portas = +router.query.portas;
+    const temPresente = +router.query.temPresente;
+    setPortas(criarPortas(portas, temPresente));
+  }, [router?.query])
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta}
+        onChange={novaPorta => {
+          console.log(novaPorta)
+          setPortas(atualizarPortas(portas, novaPorta))
+        }} />
+    });
+  }
+
+  return (
+    <div id={styles.jogo}>
+      <div className={styles.portas}>
+        {renderizarPortas()}
+      </div>
+      <div className={styles.botoes}>
+        <Link href="/">
+          <button>Reiniciar Jogo</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
