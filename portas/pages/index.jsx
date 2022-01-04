3384b1e6acfb50839aaa58f10db3e7d508f245b9
laerthnardelli <laerthnardelli@gmail.com>
2022-01-04 15:51:51 -0300

    Integrating with TypeScript

--- portas/pages/index.jsx
+++ portas/pages/index.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import Porta from "../components/Porta";
 import Presente from "../components/Presente";
+import PortaModel from "../model/porta";
 
 
 export default function Home() {
+
+  const [p1, setP1] = useState(new PortaModel(1));
+
   return (
     <div style={{ display: "flex" }}>
       {/* <Presente /> */}
-      <Porta selecionada={true} />
-      <Porta selecionada={false} />
+      <Porta porta={p1} />
+
     </div>
   )
 }
