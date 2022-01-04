05ad852f938f660d3470d9030bd1d265e411f69b
laerthnardelli <laerthnardelli@gmail.com>
2022-01-04 20:18:52 -0300

    Component Jogo #01

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -1,26 +1,9 @@
-import { useState } from "react";
-import Porta from "../components/Porta";
-import { atualizarPortas, criarPortas } from "../functions/portas";
-import PortaModel from "../model/porta";
-
 
 export default function Home() {
 
-  const [portas, setPortas] = useState(criarPortas(4, 3));
-
-  function renderizarPortas() {
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta}
-        onChange={novaPorta => {
-          console.log(novaPorta)
-          setPortas(atualizarPortas(portas, novaPorta))
-        }} />
-    });
-  }
-
   return (
     <div style={{ display: "flex" }}>
-      {renderizarPortas()}
+      <h1>Formulário de Inicio!!!</h1>
     </div>
   );
 }
---
+++ portas/pages/jogo.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Porta from "../components/Porta";
+import { atualizarPortas, criarPortas } from "../functions/portas";
+import styles from "../styles/Jogo.module.css";
+
+export default function Jogo() {
+
+  const [portas, setPortas] = useState(criarPortas(10, 3));
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta}
+        onChange={novaPorta => {
+          console.log(novaPorta)
+          setPortas(atualizarPortas(portas, novaPorta))
+        }} />
+    });
+  }
+
+  return (
+    <div id={styles.jogo}>
+      <div className={styles.portas}>
+      {renderizarPortas()}
+      </div>
+      <div className={styles.botoes}>
+
+      </div>
+    </div>
+  );
+}
