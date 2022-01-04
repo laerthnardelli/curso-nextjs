8902463fb9aa508adc99fce04d20d48a1b0cbf77
laerthnardelli <laerthnardelli@gmail.com>
2022-01-04 20:25:17 -0300

    Component Jogo #02

--- portas/pages/jogo.tsx
+++ portas/pages/jogo.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Porta from "../components/Porta";
 import { atualizarPortas, criarPortas } from "../functions/portas";
 import styles from "../styles/Jogo.module.css";
+import Link from "next/link";
 
 export default function Jogo() {
 
@@ -23,7 +24,9 @@ export default function Jogo() {
       {renderizarPortas()}
       </div>
       <div className={styles.botoes}>
-
+        <Link href="/">
+          <button>Reiniciar Jogo</button>
+        </Link>
       </div>
     </div>
   );
