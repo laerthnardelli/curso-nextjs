31a151c579031560440045da6e3d20ed2819b9e8
laerthnardelli <laerthnardelli@gmail.com>
2022-01-05 15:43:38 -0300

    Validating Form

--- portas/pages/jogo/[portas]/[temPresente].tsx
+++ portas/pages/jogo/[portas]/[temPresente].tsx
@@ -9,7 +9,18 @@ export default function jogo() {
 
   const router = useRouter();
 
+  const [valido, setValido] = useState(false)
   const [portas, setPortas] = useState([]);
+
+  useEffect(() => {
+    const portas = +router.query.portas;
+    const temPresente = +router.query.temPresente;
+
+    const qtdePortasValida = portas >= 3 && portas <= 100;
+    const temPresenteValido = temPresente >= 1 && temPresente <= portas;
+
+    setValido(qtdePortasValida && temPresenteValido)
+  }, [portas, router.query.portas, router.query.temPresente]);
 
   useEffect(() => {
     const portas = +router.query.portas;
@@ -30,7 +41,10 @@ export default function jogo() {
   return (
     <div id={styles.jogo}>
       <div className={styles.portas}>
-        {renderizarPortas()}
+        {valido ?
+          renderizarPortas() :
+          <h1>Valores inválidos</h1>
+        }
       </div>
       <div className={styles.botoes}>
         <Link href="/">
