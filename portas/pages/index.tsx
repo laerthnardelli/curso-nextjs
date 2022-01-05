4a3e20bba2c6c82a8d2e42ee88883d9f50430af1
laerthnardelli <laerthnardelli@gmail.com>
2022-01-05 15:36:11 -0300

    Finishing the Form

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -1,8 +1,12 @@
 import Cartao from "../components/Cartao";
 import styles from "../styles/Formulario.module.css"
 import Link from "next/link";
+import EntradaNumerica from "../components/EntradaNumerica";
+import { useState } from "react";
 
-export default function Formumario() {
+export default function Formulario() {
+  const [qtdePortas, setQtdePortas] = useState(3);
+  const [comPresente, setComPresente] = useState(1);
 
   return (
     <div className={styles.formulario}>
@@ -10,17 +14,24 @@ export default function Formumario() {
         <Cartao bgcolor="#c0392c">
           <h1>Monty Hall</h1>
         </Cartao>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica text="Qtde Portas?"
+            value={qtdePortas}
+            onChange={novaQtde => setQtdePortas(novaQtde)} />
+        </Cartao>
       </div>
       <div>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica text="Porta com Presente?"
+            value={comPresente}
+            onChange={novaPortaComPresente => setComPresente(novaPortaComPresente)} />
+        </Cartao>
         <Cartao bgcolor="#28a085">
-          <Link href={`/jogo/4/2`}>
+          <Link href={`/jogo/${qtdePortas}/${comPresente}`} >
             <h2 className={styles.link}>Iniciar</h2>
           </Link>
         </Cartao>
       </div>
-
     </div>
   );
 }
