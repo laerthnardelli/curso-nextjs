8807a60d26c5bc867089d953f1b859358ab5f434
laerthnardelli <laerthnardelli@gmail.com>
2022-01-06 11:17:15 -0300

    Component Static #02

---
+++ renderizacao/pages/estatico2.jsx
@@ -0,0 +1,19 @@
+export function getStaticProps() {
+  return {
+    props: {
+      numero: Math.random()
+    }
+  }
+}
+
+//npm run build
+//npm start
+
+export default function Estatico2(props) {
+  return (
+    <div>
+      <h1>Estático #02</h1>
+      <h2>{props.numero}</h2>
+    </div>
+  );
+}
