7ecfe4f53d3768297ecd42a05e2d62e60f0bc7db
laerthnardelli <laerthnardelli@gmail.com>
2022-01-07 19:48:05 -0300

    Using getStaticPaths - Part 2

--- renderizacao/pages/alunos/[id].jsx
+++ renderizacao/pages/alunos/[id].jsx
@@ -1,25 +1,50 @@
-export function getStaticPaths() {
+export async function getStaticPaths() {
+  const resp = await fetch('http://localhost:3000/api/alunos/tutores');
+  const ids = await resp.json();
+
+  const paths = ids.map(id => {
+    return { params: { id: `${id}` } }
+  });
+
   return {
-    fallback: false, //erro 404 quando não encontrar o id específico
-    paths: [
-      { params: { id: '107' } },
-      { params: { id: '203' } },
-      { params: { id: '1345' } },
-    ]
+    fallback: false, //false -> erro 404 quando não encontrar o id específico
+    paths
   }
 }
 
 
-export function getStaticProps() {
+// export async function getStaticProps(context) {
+//   const resp = await fetch(`http://localhost:3000/api/alunos/${context.params.id}`);
+//   const aluno = await resp.json();
+//   return {
+//     props: {
+//       aluno
+//     }
+//   }
+// }
+
+
+export async function getStaticProps({ params }) {
+  const resp = await fetch(`http://localhost:3000/api/alunos/${params.id}`);
+  const aluno = await resp.json();
+
   return {
-    props: {}
+    props: {
+      aluno
+    }
   }
 }
 
-export default function AlunoPorId() {
+export default function AlunoPorId(props) {
+  const { aluno } = props;
   return (
     <div>
       <h1>Detalhes do Aluno</h1>
+      <ul>
+        <li>{aluno.id}</li>
+        <li>{aluno.nome}</li>
+        <li>{aluno.email}</li>
+      </ul>
     </div>
   );
 }
