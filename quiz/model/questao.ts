9e3225237e302c0f8c907bd449162a8fafb536b0
laerthnardelli <laerthnardelli@gmail.com>
2022-01-10 17:07:00 -0300

    Shuffle Items

--- quiz/model/questao.ts
+++ quiz/model/questao.ts
@@ -1,3 +1,4 @@
+import { embaralhar } from "../functions/arrays";
 import RespostaModel from "./resposta";
 
 export default class QuestaoModel {
@@ -35,6 +36,15 @@ export default class QuestaoModel {
       if (resposta.revelada) return true;
     }
     return false;
+  }
+
+  // responderCom(indice: number): QuestaoModel {
+  //   return;
+  // }
+
+  embaralharRespostas(): QuestaoModel {
+    let respostasEmbaralhadas = embaralhar(this.#respostas)
+    return new QuestaoModel(this.#id, this.#enunciado, respostasEmbaralhadas, this.#acertou)
   }
 
   paraObjeto() {
