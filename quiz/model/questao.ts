ea4d83cfe286be799c85fc5bc787cca44cef7183
laerthnardelli <laerthnardelli@gmail.com>
2022-01-10 14:19:30 -0300

    Class "Resposta"

--- quiz/model/questao.ts
+++ quiz/model/questao.ts
@@ -1,11 +1,13 @@
+import RespostaModel from "./resposta";
+
 export default class QuestaoModel {
   #id: number
   #enunciado: string
-  #respostas: any[]
+  #respostas: RespostaModel[]
   #acertou: boolean
   //#respondida: boolean
 
-  constructor(id: number, enunciado: string, respostas: any[], acertou = false) {
+  constructor(id: number, enunciado: string, respostas: RespostaModel[], acertou = false) {
     this.#id = id;
     this.#enunciado = enunciado;
     this.#respostas = respostas;
@@ -29,8 +31,10 @@ export default class QuestaoModel {
   }
 
   get respondida() {
-    //FIXME: implementar esse método
-    return false
+    for (let resposta of this.#respostas) {
+      if (resposta.revelada) return true;
+    }
+    return false;
   }
 
 }
