eccced7ffe597609f87d98de59a2336aa7cfb7ad
laerthnardelli <laerthnardelli@gmail.com>
2022-01-12 12:26:02 -0300

    Component "Resposta" #07

--- quiz/components/Resposta.tsx
+++ quiz/components/Resposta.tsx
@@ -16,18 +16,35 @@ export default function Resposta(props: RespostaProps) {
     <div className={styles.resposta}
       onClick={() => props.respostaFornecida(props.indice)}>
       <div className={styles.conteudoResposta}>
-        <div className={styles.frente}>
-          <div className={styles.letra} style={{ backgroundColor: props.corFundoLetra }}>
-            {props.letra}
-          </div>
-          <div className={styles.valor}>
-            {resposta.valor}
+
+        {!resposta.revelada ? (
+          <div className={styles.frente}>
+            <div className={styles.letra}
+              style={{ backgroundColor: props.corFundoLetra }}>
+              {props.letra}
+            </div>
+            <div className={styles.valor}>
+              {resposta.valor}
+            </div>
           </div>
 
-        </div>
-        <div className={styles.verso}>
+        ) : (
+          <div className={styles.verso}>
+            {resposta.certa ? (
+              <div className={styles.certa}>
+                <div>A resposta certa é...</div>
+                <div className={styles.valor} >{resposta.valor}</div>
+              </div>
 
-        </div>
+            ) : (
+              <div className={styles.errada}>
+                <div>A resposta certa errada...</div>
+                <div className={styles.valor} >{resposta.valor}</div>
+              </div>
+            )}
+          </div>
+
+        )}
 
       </div>
 
