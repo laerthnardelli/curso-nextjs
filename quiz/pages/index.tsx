dd6ce03efc9d638afe82d91c5989f907badc3c97
Laerth Knodt Nardelli <laerthnardelli@gmail.com>
2022-01-17 10:14:44 -0300

    Component Index #01

--- quiz/pages/index.tsx
+++ quiz/pages/index.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import QuestaoModel from '../model/questao';
 import RespostaModel from '../model/resposta';
 import Questionario from '../components/Questionario';
@@ -11,9 +11,34 @@ const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
   RespostaModel.certa('Preta'),
 ]);
 
+const BASE_URL = 'http://localhost:3000/api/';
+
+
 export default function Home() {
 
-  const [questao, setQuestao] = useState(questaoMock);
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([]);
+  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock);
+
+  async function carregarIdsDasQuestoes() {
+    const resp = await fetch(`${BASE_URL}/questionario`);
+    const idsDasQuestoes = await resp.json();
+    console.log(idsDasQuestoes);
+    setIdsDasQuestoes(idsDasQuestoes);
+  }
+
+  async function carregarQuestoes(idQuestao: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`)
+    const json = await resp.json();
+    console.log(json);
+  }
+
+  useEffect(() => {
+    carregarIdsDasQuestoes();
+  }, []);
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestoes(idsDasQuestoes[0])
+  }, [idsDasQuestoes]);
 
   function questaoRespondida(questao: QuestaoModel) {
 
@@ -24,20 +49,11 @@ export default function Home() {
   }
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-    }}>
-      <Questionario
-        questao={questao}
-        ultima={false}
-        questaoRespondida={questaoRespondida}
-        irPraProximoPasso={irPraProximoPasso}
-      />
-
-    </div>
+    <Questionario
+      questao={questao}
+      ultima={false}
+      questaoRespondida={questaoRespondida}
+      irPraProximoPasso={irPraProximoPasso}
+    />
   );
 }
