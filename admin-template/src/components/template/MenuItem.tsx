c4a4ced52e17b1df81e8d55d5bda99d2b5024d49
Laerth Knodt Nardelli <laerthnardelli@gmail.com>
2022-01-18 16:36:03 -0300

    Component Side Menu #2 - "Menu Lateral"

--- admin-template/src/components/template/MenuItem.tsx
+++ admin-template/src/components/template/MenuItem.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 interface MenuItemProps {
     url: string
@@ -7,8 +8,18 @@ interface MenuItemProps {
 
 export default function MenuItem(props: MenuItemProps) {
     return (
-        <li className="">
-            {props.icone}
+        <li className={`hover:bg-gray-100`}>
+            <Link href={props.url}>
+                <a className={`
+                    flex flex-col justify-center items-center
+                    h-20 w-20
+                `}>
+                    {props.icone}
+                    <span className={`text-xs font-light text-gray-600`}>
+                        {props.texto}
+                    </span>
+                </a>
+            </Link>
         </li>
     );
 }
