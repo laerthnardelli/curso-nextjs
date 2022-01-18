a54bbeb9f5edca51f7aa74bb4c2f182e6f03baee
Laerth Knodt Nardelli <laerthnardelli@gmail.com>
2022-01-18 20:55:46 -0300

    Context API #1 - Ex: Consumer and useContext

--- admin-template/src/pages/notificacoes.tsx
+++ admin-template/src/pages/notificacoes.tsx
@@ -1,12 +1,25 @@
-import Layout from "../components/template/Layout";
 
+
+
+import Layout from "../components/template/Layout";
+import { AppConsumer } from "../data/context/AppContext";
+import useAppData from "../data/hook/useAppData";
 
 export default function Notificacoes() {
+
+  const dados = useAppData();
+
   return (
     <Layout
       titulo={"Notificações"}
       subtitulo={"Aqui você irá gerenciar as suas notificações"}>
-      <h3>Conteúdo!!!</h3>
+
+      <AppConsumer>
+        {dados => <h3>{dados.nome}</h3>}
+      </AppConsumer>
+
+      <h3>{dados.nome}</h3>
+
     </Layout>
   );
 }
