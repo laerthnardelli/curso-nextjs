418dbfef45129ddcd2468526247f036132281294
Laerth Knodt Nardelli <laerthnardelli@gmail.com>
2022-01-18 11:18:40 -0300

    Initial structure of the component "template"

--- admin-template/src/pages/index.tsx
+++ admin-template/src/pages/index.tsx
@@ -1,11 +1,10 @@
+import Layout from "../components/template/Layout";
 
 
 export default function Home() {
   return (
-    <div className={`
-    h-screen
-    bg-gradient-to-r from-green-500 to-blue-500`}>
-      <h1>Admin</h1>
-    </div>
-  )
+    <Layout titulo={"Página Inicial"} subtitulo={"Estamos construindo um template Admin!"}>
+      <h3>Conteúdo!!!</h3>
+    </Layout>
+  );
 }
