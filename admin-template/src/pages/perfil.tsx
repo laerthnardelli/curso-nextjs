7ca28b7680bb158a0f43c2ad761ae91c5b37a378
Laerth Knodt Nardelli <laerthnardelli@gmail.com>
2022-01-19 16:51:09 -0300

    User Avatar

---
+++ admin-template/src/pages/perfil.tsx
@@ -0,0 +1,13 @@
+
+import Layout from "../components/template/Layout";
+
+export default function Perfil() {
+
+  return (
+    <Layout
+      titulo={"Perfil do Usuário"}
+      subtitulo={"Administre as sus informações de usuário!"}>
+      <h1>Perfil do Usuário</h1>
+    </Layout>
+  );
+}
