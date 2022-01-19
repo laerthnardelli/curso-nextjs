0ca7b87053eb1c80aca3f45ac7b1d0d23829a2fa
Laerth Knodt Nardelli <laerthnardelli@gmail.com>
2022-01-18 21:21:51 -0300

    Context API #2

--- admin-template/src/pages/notificacoes.tsx
+++ admin-template/src/pages/notificacoes.tsx
@@ -4,13 +4,17 @@ import useAppData from "../data/hook/useAppData";
 
 export default function Notificacoes() {
 
-  const ctx = useAppData();
+  //const ctx = useAppData();
+  const { tema, alternarTema } = useAppData();
 
   return (
     <Layout
       titulo={"Notificações"}
       subtitulo={"Aqui você irá gerenciar as suas notificações"}>
-      <h3>{ctx.nome}</h3>
+      {/* <h3>{ctx.tema}</h3>
+      <button onClick={ctx.alternarTema} >Click</button> */}
+      {/* <h3>{tema}</h3> */}
+      <button onClick={alternarTema}>Alternar Tema</button>
     </Layout>
   );
 }
