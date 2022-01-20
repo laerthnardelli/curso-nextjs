c686649c3237b4ba2e5985b7cf66f5ac270da145
Laerth Knodt Nardelli <laerthnardelli@gmail.com>
2022-01-20 14:32:49 -0300

    Protecting Routes #4

--- admin-template/src/components/template/Layout.tsx
+++ admin-template/src/components/template/Layout.tsx
@@ -1,6 +1,5 @@
 import useAppData from "../../data/hook/useAppData";
-import forcarAutenticacao from "../../functions/ForcarAutenticacao";
-//import ForcarAutenticacao from "../auth/ForcarAutenticacao";
+import ForcarAutenticacao from "../auth/ForcarAutenticacao";
 import Cabecalho from "./Cabecalho";
 import Conteudo from "./Conteudo";
 import MenuLateral from "./MenuLateral";
@@ -15,18 +14,17 @@ export default function Layout(props: LayoutProps) {
 
     const { tema } = useAppData();
 
-    return forcarAutenticacao(
-
-        <div className={`${tema} flex h-screen w-screen`}>
-            <MenuLateral />
-            <div className={` flex flex-col w-full p-7
+    return (
+        <ForcarAutenticacao>
+            <div className={`${tema} flex h-screen w-screen`}>
+                <MenuLateral />
+                <div className={` flex flex-col w-full p-7
             bg-gray-300 dark:bg-gray-800
             `}>
-                <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
-                <Conteudo>{props.children}</Conteudo>
+                    <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
+                    <Conteudo>{props.children}</Conteudo>
+                </div>
             </div>
-        </div>
-
-
+        </ForcarAutenticacao>
     );
 }
