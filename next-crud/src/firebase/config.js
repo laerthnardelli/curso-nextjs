ae13b4c2581f440dae87b1667d52fdb010acfd6d
Laerth Knodt Nardelli <laerthnardelli@gmail.com>
2022-01-21 15:25:56 -0300

    added config firebase

---
+++ next-crud/src/firebase/config.js
@@ -0,0 +1,15 @@
+
+import firebase from "firebase/app";
+import 'firebase/auth';
+
+//npm i firebase@8.8.0
+
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    });
+}
+
+export default firebase;
